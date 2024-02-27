b9a3a4233ce788de4770eb08838aa7fba7737273
Olga Arkhipova <o.arkhipova.analyst@yandex.ru>
2024-02-27 15:53:39 +0300

    initial

---
+++ main.py
@@ -0,0 +1,45 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message
+
+BOT_TOKEN = '6749046601:AAHA2vdBASAXDm5X7qhV5pgmoJZAEebHibQ'
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+# Этот хэндлер будет срабатывать на команду "/start"
+async def process_start_command(message: Message):
+    #await bot.send_message(chat_id='199487872', text='Привет. я - бот')
+    #await bot.send_message(message.chat.id, message.text)
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+async def send_echo(message: Message):
+    try:
+        print(message)
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
+
+
+# Регистрируем хэндлеры
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_echo)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
